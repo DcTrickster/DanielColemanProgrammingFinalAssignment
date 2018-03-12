f1b4259324c6a225907144bb2efe5048cde121a5
Daniel Coleman <dc9.6@hotmail.com>
2018-03-12 23:18:06 +0000

    Scripts Added

---
+++ StarWarsBattle/Assets/Scripts/Pursue.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+using System.Text;
+
+public class Pursue : SteeringBehavior 
+{
+
+	public Boid target;
+	Vector3 targetPos;
+
+	public override Vector3 Calculate ()
+	{
+		float dist = Vector3.Distance (target.transform.position, transform.position);
+		float time = dist / boid.maxSpeed;
+		targetPos = target.transform.position + (time * target.velocity);
+		return boid.SeekForce (targetPos);
+	}
+}
